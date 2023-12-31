25a262dba5dd948fc866890e215ec905f815f4a9
ranichate28 <147973506+ranichate28@users.noreply.github.com>
2023-12-31 10:02:11 +0530

    Project architecture

---
+++ Source/logger.py
@@ -0,0 +1,18 @@
+import os
+import logging
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%M_%D_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs" , LOG_FILE)
+
+os.makedirs(logs_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name) - %levelname)s - %(message)
+    )
+
+
+
